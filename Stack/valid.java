43bae0cf57c55225c2145379c7c1db40bea28861
Vandana <vandanasharmajewar007@gmail.com>
2025-02-06 19:43:04 +0530

    Done

---
+++ Stack/valid.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class valid {
+    public static boolean parentheses(String s){
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) {
+                    return false; 
+                }
+                
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') || 
+                    (ch == '}' && top != '{') || 
+                    (ch == ']' && top != '[')) {
+                    return false; 
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        System.out.println(parentheses("(){}[]"));  
+        System.out.println(parentheses("([)]"));    
+        System.out.println(parentheses("{[()]}"));  
+        System.out.println(parentheses("((()))"));  
+        System.out.println(parentheses("(()"));     
+    }
+}
