acd5bd1628a774e96c0e79ccf72967f685b4e6e9
Vandana <vandanasharmajewar007@gmail.com>
2025-02-09 21:17:52 +0530

    done

---
+++ BST/Delete.java
@@ -0,0 +1,37 @@
+public class Delete {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+        public static Node insert(Node root, int val) {
+            if (root == null) {
+                return new Node(val);
+            }
+    
+            if (val < root.data) {
+                root.left = insert(root.left, val);
+            } else {
+                root.right = insert(root.right, val);
+            }
+            return root;
+        }
+        public class Node delete1(Node root, int val);
+    
+    public static void main(String[] args) {
+        Node root = null;
+        root = insert(root, 5);
+        root = insert(root, 3);
+        root = insert(root, 7);
+        root = insert(root, 2);
+        root = insert(root, 4);
+        root = insert(root, 6);
+        root = insert(root, 8);
+    
+}
+}
