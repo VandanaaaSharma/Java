d9c1688ceb10b0af22ef6a0847f45133bec7fa89
Vandana <vandanasharmajewar007@gmail.com>
2025-02-09 21:08:21 +0530

    Done

---
+++ BST/Build.java
@@ -0,0 +1,49 @@
+
+public class Build {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            return new Node(val);
+        }
+
+        if (val < root.data) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    
+
+    public static void inorderTraversal(Node root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            System.out.print(root.data + " ");
+            inorderTraversal(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = null;
+        root = insert(root, 5);
+        root = insert(root, 3);
+        root = insert(root, 7);
+        root = insert(root, 2);
+        root = insert(root, 4);
+        root = insert(root, 6);
+        root = insert(root, 8);
+
+        System.out.println("Inorder traversal of the BST:");
+        inorderTraversal(root);
+    }
+}
