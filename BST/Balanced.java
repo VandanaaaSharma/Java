7db80e8c3d8fa820758c836b0891dc17d6ed3cb1
Vandana <vandanasharmajewar007@gmail.com>
2025-02-10 12:23:57 +0530

    done

---
+++ BST/Balanced.java
@@ -0,0 +1,42 @@
+public class Balanced {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static Node createBST(int arr[], int st, int end) {
+        if (st > end) {
+            return null;
+        }
+        int mid = (st + end) / 2;
+        Node root = new Node(arr[mid]);
+
+        root.left = createBST(arr, st, mid - 1);
+        root.right = createBST(arr, mid + 1, end);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        Node root = createBST(arr, 0, arr.length - 1);
+
+        System.out.println("Preorder traversal of Balanced BST:");
+        preorder(root);
+    }
+}
